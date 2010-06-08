9e071c57b852533f286aad99ca5c6a3f02361179
evizitei <ethan.vizitei@gmail.com>
2010-06-08 13:48:05 -0400

    include active support (this is supposed to be a rails gem)
    Signed-off-by: evizitei <ethan.vizitei@gmail.com>

--- test/helper.rb
+++ test/helper.rb
@@ -1,4 +1,14 @@
 require 'rubygems'
+
+if RUBY_VERSION >= '1.9'
+  require 'time'
+  require 'date'
+  require 'active_support/time'
+else
+  require 'active_support'
+  require 'active_support/core_ext'
+end
+
 require 'test/unit'
 require 'shoulda'
 
