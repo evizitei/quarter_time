fea432e6b166fbc65643e2a67bd95b3b4a3ca70c
evizitei <ethan.vizitei@gmail.com>
2010-06-08 13:50:09 -0400

    Quarter object and test
    Signed-off-by: evizitei <ethan.vizitei@gmail.com>

---
+++ lib/quarter_time/quarter.rb
@@ -0,0 +1,58 @@
+class Quarter
+  attr_accessor :year
+  attr_accessor :quarter
+  
+  def initialize(year,quarter)
+    @year = year
+    @quarter = quarter
+  end
+  
+  def next
+    @next ||= build_next
+  end
+  
+  def previous
+    @previous ||= build_previous
+  end
+  
+  def start_date
+    Date.parse("#{(@quarter * 3) - 2}/01/#{@year}")
+  end
+
+  def end_date
+    start_date + 3.months - 1.day
+  end
+  
+  def yq_hash   
+    {:year=>self.year,:quarter=>self.quarter}
+  end
+  
+  def date_hash 
+    {:start_date=>self.start_date,:end_date=>self.end_date}
+  end
+  
+  def date_strings_hash
+    {:start_date=>self.start_date.strftime("%m/%d/%Y"),:end_date=>self.end_date.strftime("%m/%d/%Y")}
+  end
+  
+  def quarter_stamp
+    "Q#{self.quarter}, #{self.year}"
+  end
+  
+protected
+  def build_next
+    if @quarter < 4
+      Quarter.new(@year,@quarter + 1)
+    else
+      Quarter.new(@year + 1,1)
+    end
+  end
+  
+  def build_previous
+    if @quarter > 1
+      Quarter.new(@year,@quarter - 1)
+    else
+      Quarter.new(@year - 1,4)
+    end
+  end
+end
---
+++ test/test_quarter.rb
@@ -0,0 +1,53 @@
+require 'helper'
+
+class TestQuarter < Test::Unit::TestCase
+  context "Quarter" do
+    should "calculate start date for first quarter" do
+      assert_equal Date.parse("01/01/2009"),Quarter.new(2009,1).start_date
+    end
+    
+    should "calculate start date for second quarter" do
+      assert_equal Date.parse("04/01/2009"),Quarter.new(2009,2).start_date
+    end
+    
+    should "calculate start date for third quarter" do
+      assert_equal Date.parse("07/01/2009"),Quarter.new(2009,3).start_date
+    end
+    
+    should "calculate start date for fourth quarter" do
+      assert_equal Date.parse("10/01/2009"),Quarter.new(2009,4).start_date
+    end
+    
+    should "calculate end date for first quarter" do
+      assert_equal Date.parse("03/31/2009"),Quarter.new(2009,1).end_date
+    end
+    
+    should "calculate end date for second quarter" do
+      assert_equal Date.parse("06/30/2009"),Quarter.new(2009,2).end_date
+    end
+    
+    should "calculate end date for third quarter" do
+      assert_equal Date.parse("09/30/2009"),Quarter.new(2009,3).end_date
+    end
+    
+    should "calculate end date for fourth quarter" do
+      assert_equal Date.parse("12/31/2009"),Quarter.new(2009,4).end_date
+    end   
+  end     
+  
+  context "Prebuilt Quarter" do
+    setup { @quarter = Quarter.new(2009,4)}
+    
+    should "produce a hash for parameters with year and month" do
+      assert_equal({:year=>2009,:quarter=>4},@quarter.yq_hash)
+    end
+    
+    should "produce a hash for parameters with start_date and end_date" do
+      assert_equal({:start_date=>Date.parse("10/01/2009"),:end_date=>Date.parse("12/31/2009")},@quarter.date_hash)
+    end    
+    
+    should "produce a params hash with strings for dates" do
+      assert_equal({:start_date=>"10/01/2009",:end_date=>"12/31/2009"},@quarter.date_strings_hash)
+    end
+  end
+end
