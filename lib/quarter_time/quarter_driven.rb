f92958efe2c1e992af8825254d801f3b2a22697f
t.pickett66 <t.pickett66@gmail.com>
2010-11-11 16:22:03 -0600

    Update scope creation to use scope for ASv3+ and named_scope for 3-
    Signed-off-by: t.pickett66 <t.pickett66@gmail.com>

--- lib/quarter_time/quarter_driven.rb
+++ lib/quarter_time/quarter_driven.rb
@@ -1,8 +1,12 @@
 module QuarterTime
   module QuarterDriven
     def self.included(base)
-      base.class_eval do
-        named_scope :for_quarter, lambda {|*args|
+      version = ActiveSupport::VERSION::MAJOR
+      scope_method = "named_scope" if version.to_i < 3
+      scope_method = "scope" if version.to_i >= 3
+       
+      base.class_eval %Q{
+        #{scope_method} :for_quarter, lambda {|*args|
           quarter_obj = (args[0].is_a?(Quarter) ? args[0] : Quarter.new(args[0],args[1]))
           {:conditions=>{:year=>quarter_obj.year, :quarter=>quarter_obj.quarter}}
         }
@@ -11,7 +15,7 @@ module QuarterTime
         [:start_date,:end_date,:quarter_stamp].each do |delegated_method|
           define_method(delegated_method){ self.quarter_obj.send(delegated_method) }
         end
-      end
+      }
     end
     #   
     def quarter_obj
